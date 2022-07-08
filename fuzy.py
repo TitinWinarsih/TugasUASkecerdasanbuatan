17b7f067604ede8212f9eae67697c9459400d3ae
TitinWinarsih <103985678+TitinWinarsih@users.noreply.github.com>
2022-07-08 16:29:13 +0700

    Add files via upload

---
+++ fuzy.py
@@ -0,0 +1,124 @@
+
+class BentukAlbum():
+    kotak = 30
+    bulat = 20
+    persegipanjang = 60
+
+    def turun(self, x):
+        if x <= self.kotak:
+            return 0
+        elif x >= self.bulat:
+            return 1
+        else:
+            return bawah(x, self.bulat, self.kotak)
+
+    def pas(self, x):
+        if self.kotak < x < self.bulat:
+            return atas(x, self.kotak, self.bulat)
+        elif self.segitiga < x < self.persegipanjang:
+            return bawah(x, self.bulat, self.persegipanjang)
+        elif x == self.bulat:
+            return 1
+        else:
+            return 0
+
+    def naik(self, x):
+        if x >= self.bulat:
+            return 1
+        elif x <= self.persegipanjang:
+            return 0
+        else:
+            return up(x, self.persegipanjang, self.bulat)
+
+class BahanAlbum():
+    halus = 40
+    kasar = 30
+    jelas = 30
+
+    def sedikit(self, x):
+        if x >= self.halus:
+            return 0
+        elif x <= self.kasar:
+            return 1
+        else:
+            return down(x, self.kasar, self.halus)
+    
+    def cukup(self, x):
+        if self.halus < x < self.kasar:
+            return up(x, self.halus, self.kasar)
+        elif self.kasar < x < self.jelas:
+            return down(x, self.kasar, self.jelas)
+        elif x == self.kasar:
+            return 1
+        else:
+            return 0
+
+    def banyak(self, x):
+        if x >= self.jelas:
+            return 1
+        elif x <= self.kasar:
+            return 0
+        else:
+            return up(x, self.kasar, self.jelas)
+
+class WarnaAlbum():
+    putih = 30
+    hitam = 40
+    pink = 20
+
+    def sedikit(self, x):
+        if x >= self.hitam:
+            return 0
+        elif x <= self.putih:
+            return 1
+        else:
+            return down(x, self.putih, self.hitam)
+    
+    def cukup(self, x):
+        if self.putih < x < self.hitam:
+            return up(x, self.hitam, self.putih)
+        elif self.putih < x < self.pink:
+            return down(x, self.putih, self.pink)
+        elif x == self.putih:
+            return 1
+        else:
+            return 0
+
+    def banyak(self, x):
+        if x >= self.pink:
+            return 1
+        elif x <= self.putih:
+            return 0
+        else:
+            return up(x, self.putih, self.pink)
+
+class versiAlbum():
+    repackage = 30
+    lunatic = 50
+    holiday1 = 40
+   
+    def sedikit(self, x):
+        if x >= self.lunatic:
+            return 0
+        elif x <= self.repackage:
+            return 1
+        else:
+            return down(x, self.repackage, self.lunatic)
+    
+    def cukup(self, x):
+        if self.merah < x < self.lunatic:
+            return up(x, self.repackage, self.lunatic)
+        elif self.holiday1 < x < self.holiday1:
+            return down(x, self.lunatic, self.holiday1)
+        elif x == self.lunatic:
+            return 1
+        else:
+            return 0
+
+    def banyak(self, x):
+        if x >= self.holiday1:
+            return 1
+        elif x <= self.lunatic:
+            return 0
+        else:
+            return up(x, self.lunatic, self.holiday1)
